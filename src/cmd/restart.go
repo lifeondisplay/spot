092c794509818e8f4c77cb883b883f4fb64f9734
lucas <displayonlife@gmail.com>
2024-11-19 23:57:03 -0300

    adição do comando `restart`

---
+++ src/cmd/restart.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+// restartspotify
+func RestartSpotify() {
+	switch runtime.GOOS {
+		case "windows":
+			exec.Command("taskkill", "/F", "/IM", "spotify.exe").Run()
+			exec.Command(filepath.Join(spotifyPath, "spotify.exe")).Start()
+		
+		case "linux":
+			exec.Command("pkill", "spotify").Run()
+			exec.Command(filepath.Join(spotifyPath, "spotify")).Start()
+		
+		case "darwin":
+			exec.Command("pkill", "Spotify").Run()
+			exec.Command("open", "/Applications/Spotify.app").Start()
+	}
+}
